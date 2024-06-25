9a2e4acfed4c7b797151d677ab6a4318cf1375ed
myrlyn13 <myranda.lynne13@gmail.com>
2024-06-25 05:36:27 -0400

    server.py

--- server.py
+++ server.py
@@ -6,41 +6,41 @@ This script defines a Flask-based server for performing emotion detection on use
 Author(Learner): [Myrlyn13]
 """
 
+"""
+This module will enable customers to access this application on the web.
+"""
+
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
-from EmotionDetection.emotion_detection import emotion_predictor
+from EmotionDetection import emotion_detection
 
-app = Flask("Emotion Detection")
-
-def run_emotion_detection():
-    """
-    Main function to run the Emotion Detection application.
-    """
-    app.run(host="0.0.0.0", port=5000)
+app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_detector():
+def emotion_detector():
     """
-    Analyze the user-provided text for emotions and return the result.
+    This function will take a text input from a user interface and
+    pass back the emotion score of that text input.
     """
-    text_to_detect = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_detect)
-    formatted_response = emotion_predictor(response)
-    if formatted_response['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
-    return (
-        f"For the given statement, the system response is 'anger': {formatted_response['anger']}"
-        f"'disgust': {formatted_response['disgust']}, 'fear': {formatted_response['fear']},"
-        f"'joy': {formatted_response['joy']} and 'sadness': {formatted_response['sadness']}."
-        f"The dominant emotion is {formatted_response['dominant_emotion']}."
-    )     
+    text_to_analyze = request.args.get('textToAnalyze')
+    dominant_emotion = emotion_detection.emotion_detector(text_to_analyze)
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+    
+    anger = dominant_emotion['anger']
+    disgust = dominant_emotion['disgust']
+    fear = dominant_emotion['fear']
+    joy = dominant_emotion['joy']
+    sad = dominant_emotion['sadness']
+    max_emotion = max(dominant_emotion.items(), key=lambda x: x[1])[0]
+    
+    return (f"For the given statement, the system response is "
+            f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, "
+            f"'sadness': {sad}. "
+            f"The dominant emotion is {max_emotion}")
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    """
+    This function will render the user interface
+    """
     return render_template('index.html')
-
-    if __name__ == "__main__":
-        run_emotion_detection()
